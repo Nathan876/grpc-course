05a5500ad7a9d1e5897d6c562a67431e3166e8a7
Nathan CHABALIER <Nathan@MacBook-Air-de-Thierry.local>
2026-09-23 14:56:44 +0200

    Add: Create server and client

---
+++ client.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.append('generated')
+import grpc
+from generated import user_pb2, user_pb2_grpc
+
+# 1. Un "channel" = la connexion vers le serveur (comme une prise électrique)
+with grpc.insecure_channel("localhost:50051") as channel:
+    # 2. Le stub = le client généré qui connaît les méthodes du service
+    stub = user_pb2_grpc.UserServiceStub(channel)
+    # 3. L'appel RPC : comme une fonction locale... qui traverse le réseau
+    response = stub.GetUser(user_pb2.GetUserRequest(user_id=1))
+    print(f"Nom : {response.user.name}")
+    print(f"Email : {response.user.email}")
